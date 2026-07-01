b3f4999eda17a84d7d6d7e00fa01fd3ef7d44eb4
jeanhrm <ingempjordonez@gmail.com>
2026-06-30 23:52:21 -0500

    cors abierto para pruebas

--- config/cors.php
+++ config/cors.php
@@ -3,11 +3,7 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [
-    'https://jeanhrm.github.io',
-    'https://jeanhrm.github.io/consultaunicef',
-    'https://jeanhrm.github.io/consultaunicef/',
-],
+    'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
