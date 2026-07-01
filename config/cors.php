de25ea25365020cff33d1eeffc629da7ca239763
jeanhrm <ingempjordonez@gmail.com>
2026-06-30 23:22:26 -0500

    fix cors origen github pages

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,11 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://jeanhrm.github.io/consultaunicef/'],
+    'allowed_origins' => [
+    'https://jeanhrm.github.io',
+    'https://jeanhrm.github.io/consultaunicef',
+    'https://jeanhrm.github.io/consultaunicef/',
+],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
