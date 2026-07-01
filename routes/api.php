9683c0d646e38484de4bed64f03e12c00f2a57e7
jeanhrm <ingempjordonez@gmail.com>
2026-06-30 23:40:29 -0500

    ruta test temporal

--- routes/api.php
+++ routes/api.php
@@ -7,3 +7,14 @@ Route::middleware('throttle:60,1')->group(function () {
     Route::post('/validar-codigo', [RespuestaController::class, 'validarCodigo']);
     Route::post('/respuestas', [RespuestaController::class, 'store']);
 });
+
+Route::get('/test-codigo', function() {
+    $codigo = '0421255';
+    $ie = \Illuminate\Support\Facades\DB::table('instituciones')
+             ->where('codigo_modular', $codigo)
+             ->first();
+    return response()->json([
+        'buscado' => $codigo,
+        'encontrado' => $ie,
+    ]);
+});
